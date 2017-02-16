7ff9c8af563226ff4c317b15de3c1897aea0014f
SavageCore <talk@savagecore.eu>
2017-02-16 10:43:41 +0000

    Switch to XO

--- app/main.js
+++ app/main.js
@@ -3,44 +3,44 @@ const url = require('url');
 
 const {
 app,
-BrowserWindow,
+BrowserWindow
 } = require('electron');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 350,
-    height: 210,
-    frame: true,
-    backgroundColor: '#212121',
-    show: false,
-  });
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-  mainWindow.setMaximizable(false);
-  mainWindow.setMenu(null);
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
+	mainWindow = new BrowserWindow({
+		width: 350,
+		height: 210,
+		frame: true,
+		backgroundColor: '#212121',
+		show: false
+	});
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+	mainWindow.setMaximizable(false);
+	mainWindow.setMenu(null);
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+	});
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
--- app/renderer.js
+++ app/renderer.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const {
 app,
-shell,
+shell
 } = require('electron').remote;
 const JSZip = require('jszip');
 const walk = require('walk');
@@ -10,101 +10,101 @@ const walk = require('walk');
 /* global document */
 
 function ensureDirectoryExistence(filePath) {
-  const dirname = path.dirname(filePath);
-  if (fs.existsSync(dirname)) {
-    return;
-  }
-  ensureDirectoryExistence(dirname);
-  fs.mkdirSync(dirname);
+	const dirname = path.dirname(filePath);
+	if (fs.existsSync(dirname)) {
+		return;
+	}
+	ensureDirectoryExistence(dirname);
+	fs.mkdirSync(dirname);
 }
 
 function handleDragOver(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  const element = document.getElementById('statusBox');
-  element.innerText = 'Let it go!';
+	e.stopPropagation();
+	e.preventDefault();
+	const element = document.getElementById('statusBox');
+	element.innerText = 'Let it go!';
 }
 
 function handleDragLeave(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  const element = document.getElementById('statusBox');
-  element.innerText = 'Drop client jar here';
+	e.stopPropagation();
+	e.preventDefault();
+	const element = document.getElementById('statusBox');
+	element.innerText = 'Drop client jar here';
 }
 
 function handleFileSelection(e) {
-  e.stopPropagation();
-  e.preventDefault();
+	e.stopPropagation();
+	e.preventDefault();
 
-  const inputFiles = e.dataTransfer.files;
+	const inputFiles = e.dataTransfer.files;
 
-  const newRP = new JSZip();
+	const newRP = new JSZip();
 
-  const element = document.getElementById('statusBox');
-  element.innerText = 'Extracting from jar...';
+	const element = document.getElementById('statusBox');
+	element.innerText = 'Extracting from jar...';
 
-  new JSZip.external.Promise((resolve, reject) => {
-    fs.readFile(inputFiles[0].path, (err, data) => {
-      if (err) {
-        reject(e);
-      } else {
-        resolve(data);
-      }
-    });
-  }).then(data => JSZip.loadAsync(data)).then((zip) => {
-    const tmpPath = `${app.getPath('temp')}\\erp`;
-    const folderWalker = walk.walk(tmpPath, {
-      followLinks: false,
-    });
-    zip.folder('assets').forEach((relativePath, file) => {
+	new JSZip.external.Promise((resolve, reject) => {
+		fs.readFile(inputFiles[0].path, (err, data) => {
+			if (err) {
+				reject(e);
+			} else {
+				resolve(data);
+			}
+		});
+	}).then(data => JSZip.loadAsync(data)).then(zip => {
+		const tmpPath = `${app.getPath('temp')}\\erp`;
+		const folderWalker = walk.walk(tmpPath, {
+			followLinks: false
+		});
+		zip.folder('assets').forEach((relativePath, file) => {
     // Extract all files to temp directory
-      const dest = path.join(tmpPath, file.name);
-      ensureDirectoryExistence(dest);
-      fs.writeFileSync(dest, file.nodeStream().pipe(fs.createWriteStream(dest)));
-    });
+			const dest = path.join(tmpPath, file.name);
+			ensureDirectoryExistence(dest);
+			fs.writeFileSync(dest, file.nodeStream().pipe(fs.createWriteStream(dest)));
+		});
     // Walk directories
-    folderWalker.on('directory', (root, stat, next) => {
-      element.innerText = 'Building resource pack';
-      element.className += ' loading';
-      const folder = newRP.folder(path.relative(`${app.getPath('temp')}/erp`, `${root}/${stat.name}`));
+		folderWalker.on('directory', (root, stat, next) => {
+			element.innerText = 'Building resource pack';
+			element.className += ' loading';
+			const folder = newRP.folder(path.relative(`${app.getPath('temp')}/erp`, `${root}/${stat.name}`));
       // Walk the folder for files
-      fs.readdir(`${root}/${stat.name}`, (err, files) => {
-        if (err) {
-          return;
-        }
+			fs.readdir(`${root}/${stat.name}`, (err, files) => {
+				if (err) {
+					return;
+				}
         // Walk files in directory
-        files.forEach((file) => {
-          const currentFP = `${root}/${stat.name}/${file}`;
-          if (fs.statSync(currentFP).isDirectory()) {
-            return;
-          }
-          folder.file(file, fs.readFileSync(currentFP));
-        });
-      });
-      next();
-    });
-    folderWalker.on('end', () => {
-      const mcmeta = `${app.getAppPath()}/assets/pack.mcmeta`;
-      const packimg = `${app.getAppPath()}/assets/pack.png`;
-      if (fs.existsSync(mcmeta)) {
-        newRP.file('pack.mcmeta', fs.readFileSync(mcmeta));
-      }
-      if (fs.existsSync(packimg)) {
-        newRP.file('pack.png', fs.readFileSync(packimg));
-      }
-      newRP.generateNodeStream({
-        type: 'nodebuffer',
-        streamFiles: true,
-      }).pipe(fs.createWriteStream('default.zip')).on('finish', () => {
-        element.innerText = 'Done!';
-        element.className = 'noselect';
-        shell.showItemInFolder(`${process.cwd()}/default_resourcepack.zip`);
-        setTimeout(() => {
-          element.innerText = 'Drop client jar here';
-        }, 3000);
-      });
-    });
-  });
+				files.forEach(file => {
+					const currentFP = `${root}/${stat.name}/${file}`;
+					if (fs.statSync(currentFP).isDirectory()) {
+						return;
+					}
+					folder.file(file, fs.readFileSync(currentFP));
+				});
+			});
+			next();
+		});
+		folderWalker.on('end', () => {
+			const mcmeta = `${app.getAppPath()}/assets/pack.mcmeta`;
+			const packimg = `${app.getAppPath()}/assets/pack.png`;
+			if (fs.existsSync(mcmeta)) {
+				newRP.file('pack.mcmeta', fs.readFileSync(mcmeta));
+			}
+			if (fs.existsSync(packimg)) {
+				newRP.file('pack.png', fs.readFileSync(packimg));
+			}
+			newRP.generateNodeStream({
+				type: 'nodebuffer',
+				streamFiles: true
+			}).pipe(fs.createWriteStream('default.zip')).on('finish', () => {
+				element.innerText = 'Done!';
+				element.className = 'noselect';
+				shell.showItemInFolder(`${process.cwd()}/default_resourcepack.zip`);
+				setTimeout(() => {
+					element.innerText = 'Drop client jar here';
+				}, 3000);
+			});
+		});
+	});
 }
 
 // Set up the file drag and drop listeners:
